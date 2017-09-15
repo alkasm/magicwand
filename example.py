8b75404b74802d5bacc078b56c6375cdc16bcbcb
alkasm <ar@reynoldsalexander.com>
2017-09-15 03:04:43 -0700

    Added keyboard controls, switching between different displays

--- example.py
+++ example.py
@@ -2,11 +2,5 @@ import cv2
 from magicwand import SelectionWindow
 
 image = cv2.imread('lane.jpg')
-window = SelectionWindow('Selection Window', image)
-window.show()
-mean, stddev = window.getMeanStdDev()
-minc, maxc = window.getMinMax()
-print('Mean Color:   ', mean,
-      '\nColor StdDev: ', stddev,
-      '\nMin Color:    ', minc,
-      '\nMax Color:    ', maxc, '\n')
+selection = SelectionWindow('Selection Window', image, connectivity=8)
+selection.show(verbose=True)
